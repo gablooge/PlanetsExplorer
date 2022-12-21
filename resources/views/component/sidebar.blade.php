83ba0ddfc39cfebcceab3c23a8fbf34142671fdc
Samsul Hadi <samsulhadikediri@gmail.com>
2022-12-21 18:56:48 +0700

    chore: icons

--- resources/views/component/sidebar.blade.php
+++ resources/views/component/sidebar.blade.php
@@ -1,17 +1,12 @@
 <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
     <div class="sidebar-brand d-none d-md-flex">
-        <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-            <use xlink:href="{{ asset('') }}assets/brand/coreui.svg#full"></use>
-        </svg>
-        <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
-            <use xlink:href="{{ asset('') }}assets/brand/coreui.svg#signet"></use>
-        </svg>
+        Planets Explorer
     </div>
     <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
         <li class="nav-item">
             <a class="nav-link" href="{{ route('planet_list') }}">
                 <svg class="nav-icon">
-                    <use xlink:href="{{ asset('') }}vendors/@coreui/icons/svg/free.svg#cil-speedometer"></use>
+                    <use xlink:href="{{ asset('') }}vendors/@coreui/icons/svg/free.svg#cil-globe-alt"></use>
                 </svg> 
                 Planets 
             </a>
@@ -19,7 +14,7 @@
         <li class="nav-item">
             <a class="nav-link" href="{{ route('logbook_list') }}">
                 <svg class="nav-icon">
-                    <use xlink:href="{{ asset('') }}vendors/@coreui/icons/svg/free.svg#cil-speedometer"></use>
+                    <use xlink:href="{{ asset('') }}vendors/@coreui/icons/svg/free.svg#cil-history"></use>
                 </svg> 
                 Logbooks 
             </a>
